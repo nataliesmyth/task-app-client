bf09d9163e8c9b66adeb12b1bc4b6b58bd576d72
Nataliesmyth <nataliesmyth11@gmail.com>
2020-09-19 18:31:46 -0400

    forms and events

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,5 +7,16 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#sign-out').hide()
+  $('#change-password').hide()
+  $('#create-task').hide()
+  $('#view-tasks').hide()
+  $('#update-task').hide()
+  $('#delete-task').hide()
+
+  // User Auth
+  // $('#sign-up-form').on('submit', authEvents.onSignUp)
+  // $('#sign-in-form').on('submit', authEvents.onSignIn)
+  // $('#change-password').on('submit', authEvents.onChangePassword)
+  // $('#sign-out').on('click', authEvents.onSignOut)
 })
