eac4f463fe858a00503534eb7fa008d1c37f1f4a
Nataliesmyth <nataliesmyth11@gmail.com>
2020-09-23 17:53:01 -0400

    heroku URL added

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://cryptic-harbor-00209.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
